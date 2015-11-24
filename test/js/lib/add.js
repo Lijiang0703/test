8177e5a6f87adc80ea3c18b390a6b84f5fcf2ee4
Lijiang0703 <li_jiang0703@outlook.con>
2015-11-24 19:28:41 +0800

    24号更新

--- test/js/lib/add.js
+++ test/js/lib/add.js
@@ -2,7 +2,8 @@ define(['jquery','board'],function($,B){
 	function add(){
 		this.type={
 			isimgadd:false,
-			istextadd:false
+			istextadd:false,
+			ispreview:false
 		}
 	}
 	add.prototype={
@@ -10,13 +11,14 @@ define(['jquery','board'],function($,B){
 			var Type=$.extend(this.type,type);
 			if(Type.istextadd){
 				B.elements('istext');
-				// $("#work").append('<input type="text" class="col-md-10 textstyle col-md-push-1" value="请输入文字"></input>');		
 				Type.istextadd=false;
 			}
 			if(Type.isimgadd){
-				B.elements('isimg');
-				// $("#work").append("<img src='img/1.jpg'/ class='col-md-10 imgstyle col-md-push-1'>");	
+				B.elements('isimg');	
 				Type.isimgadd=false;
+			}
+			if(Type.ispreview){
+				Type.ispreview=false;
 			}
 		}
 	}
--- test/js/lib/board.js
+++ test/js/lib/board.js
@@ -2,43 +2,69 @@ define(['backbone','text!template.tpl'],function(){
 	var tpl=require('text!template.tpl');
 	var tpl_text=tpl.split('<br/>')[0];
 	var tpl_img=tpl.split('<br/>')[1];
-	console.log(tpl_text);
-	function elements(obj){
-		var m=Backbone.Model.extend({
-			defaults:{
-				textvalue:"请输入文字",
-				imgsrc:"img/1.jpg"
+	var tpl_changeimg=tpl.split('<br/>')[2];
+	//获取模版
+	
+	//显示图片属性
+	var Vatt_img=Backbone.View.extend({
+		el:$('#pro_img'),
+		template:_.template(tpl_changeimg),
+		initialize:function(){
+			this.render();
+		},
+		render:function(){
+			for(var i=1;i<5;i++){
+				this.$el.append(this.template({changesrc:i}));
 			}
-		});
-		var  v_text=Backbone.View.extend({
-			el:$('#work'),
-			template:_.template(tpl_text),
-			initialize:function(){
-				this.render();
-			},
-			render:function(){
-				this.$el.append(this.template(this.model.toJSON()));
-				return this;
-			}
-		});
-		var  v_img=Backbone.View.extend({
-			el:$('#work'),
-			template:_.template(tpl_img),
-			initialize:function(){
-				this.render();
-			},
-			render:function(){
-				this.$el.append(this.template(this.model.toJSON()));
-				return this;
-			}
-		});
-		var M=new m;
-		if(obj=='istext'){
-			var V_text=new v_text({model:M});
 		}
-		if(obj=='isimg'){
-			var V_img=new v_img({model:M});
+	});
+	//model
+	var m=Backbone.Model.extend({
+		defaults:{
+			textvalue:"请输入文字",
+			imgsrc:"img/1.jpg"
+			// textid:"txt"+textcount,
+			// imgid:"img"+imgcount
 		}
+	});
+
+	var _v_img=Backbone.View.extend({
+		el:$('#work'),
+		template:_.template(tpl_img),
+		events:{
+			'click .imgstyle':'do'
+		},
+		do:function(){
+			var show=new Vatt_img();
+		},
+		add:function(model){
+			this.$el.append(this.template(model.toJSON()));
+		}
+	});
+
+	var  _v_text=Backbone.View.extend({
+		el:$('#work'),
+		template:_.template(tpl_text),
+		add:function(model){
+			this.$el.append(this.template(model.toJSON()));
+		}
+	});
+	var v_img = new _v_img();
+	var v_text= new _v_text();
+	var c_img = new Backbone.Collection;
+	var c_text= new Backbone.Collection;
+	c_img.on("add", function(model) {
+	  	v_img.add(model);
+	});
+	c_text.on("add",function(model){
+	  	v_text.add(model);
+	});
+
+	return {
+		v_img:v_img,
+		v_text:v_text,
+		c_img:c_img,
+		c_text:c_text,
+		m:m,
 	}
-	return {elements:elements}
 });	
