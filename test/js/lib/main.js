578baf721989343863818162d4066ba792263bf2
li_jiang0703@outlook.com <li_jiang0703@outlook.com>
2015-11-30 13:17:00 +0800

    update

---
+++ test/js/lib/main.js
@@ -0,0 +1,60 @@
+require.config({
+	paths:{
+		jquery:'jquery-2.1.3',
+		underscore:'underscore-min',
+		backbone:'backbone-min',
+		bootstrap:'bootstrap.min',
+		bootbox:'bootbox.min'
+	},
+	shim:{
+		'underscore':{
+			exports:'_'
+		},
+		'backbone':{
+			deps:['underscore','jquery'],
+			exports:'Backbone'
+		},
+		'bootstrap':{
+			deps:['jquery'],
+			exports:'bootstrap'
+		},
+		'bootbox':{
+			deps:['jquery','bootstrap'],
+			exports:'bootbox'
+		}
+	}
+});
+require(['jquery','board','bootbox'],function($,b,box){
+	$(function(){
+		var getstore=window.localStorage?localStorage.getItem("store"):Cookie.read("store");
+		$("#work").html(getstore);
+	});
+	$("#addimg").on('click',function(){
+		var model = new b.m();
+		b.c_img.add([model]);
+	});
+	$("#addtext").on('click',function(){
+		var model = new b.m();
+		b.c_text.add([model]);	
+	});
+	$("#work_save").on('click',function(){
+		var source=$('#work').html();
+		if(window.localStorage){
+			localStorage.removeItem("store");
+			localStorage.setItem("store",source);
+		}
+		else{
+			Cookie.write("store",source);
+		}
+	});
+	$('#work_show').on('click',function(){
+		var showsource=$('#work').html();
+		showsource.replace('textstyle','textstyle_show');
+		showsource.replace('animated','');
+		console.log(showsource);
+		bootbox.alert({
+			title:'预览',
+			message:showsource
+		});
+	});
+});
---
+++ test/js/lib/template.tpl
@@ -0,0 +1,11 @@
+<div class="row mod">
+	<span class="close animated bounceInLeft">X</span>
+	<p class="col-md-10 col-md-push-1 textstyle animated bounceInLeft"><%=textvalue%></p>
+</div>
+<br/>
+<div class="row mod">
+	<span class="close animated bounceInLeft">X</span>
+	<img src="<%=imgsrc%>" alt="" class="col-md-10 col-md-push-1 imgstyle animated bounceInLeft"/>
+</div>
+<br/>
+<img src="img/<%=changesrc%>.jpg" class="col-md-6 newimg">
